d4c18f302d5b2ddb7a79335ce4f31d7e53f1f08d
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-22 00:20:31 +0200

    sort of Updated 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,13 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+* selection_sort - Sorts integers in ascending order
+* @array: The array to be sorted
+* @size: Number of elements in the array
+* Return: Nothing (SUCCESS)
+*/
 
+void selection_sort(int *array, size_t size)
+{
+	
