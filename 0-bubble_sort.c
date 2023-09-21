285bcdd5f5e4f4c4b1ccc64839bb537721b7176a
Nmcleon <nmcleon10@gmail.com>
2023-09-21 01:11:59 -0700

    betty err

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,37 +2,38 @@
 #include "sort.h"
 
 /**
- * bubble_sort - A function for bubble sorting.
- * @array: The array to be sorted
- * @size: number of elements in array
- * Return: Nothing (SUCCESS)
- */
+* bubble_sort - A function for bubble sorting.
+* @array: The array to be sorted
+* @size: number of elements in array
+* Return: Nothing (SUCCESS)
+*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swap;
+	size_t i, j;
+	int temp;
+	int swap;
 
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        swap = 0; /* check if any swaps are made */
-
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-                swap = 1;
-                print_array(array, size); /* Print the array after each swap */
-            }
-        }
-        if (swap == 0)
-            break;
-    }
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		if (swap == 0)
+		{
+			break;
+		}
+	}
 }
--- print_array.c
+++ print_array.c
@@ -2,22 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+  * print_array - the function name
+  * @array: parameter of type const int *.
+  * @size: parameter of type size_t .
+  * Return: void .
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
