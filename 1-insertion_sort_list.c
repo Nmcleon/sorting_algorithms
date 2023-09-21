e7ae1231ee235d028deae0f77dc383a42bb5ac41
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-22 00:03:03 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,36 +7,48 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node, *next_node;
+    listint_t *node, *next_node, *prev_node, *temp;
 
-	*node = NULL;
-	*next_node = NULL;
+    if (!list || !(*list))
+        return;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    node = (*list)->next;
+    while (node)
+    {
+        next_node = node->next;
+        prev_node = node->prev;
 
-	node = *list;
-	node = node->next;
-	while (node)
-	{
-		while (node->prev && node->n < (node->prev)->n)
-		{
-			next_node = node;
-			if (node->next)
-				(node->next)->prev = next_node->prev;
-			(node->prev)->next = next_node->next;
-			node = node->prev;
-			next_node->prev = node->prev;
-			next_node->next = node;
-			if (node->prev)
-				(node->prev)->next = next_node;
-			node->prev = next_node;
-			if (next_node->prev == NULL)
-				*list = next_node;
-			print_list(*list);
-			node = node->prev;
-		}
-		node = node->next;
-	}
+        while (prev_node && prev_node->n > node->n)
+        {
+            temp = node->next;
 
+            if (prev_node->prev)
+                prev_node->prev->next = node;
+            else
+                *list = node;
+
+            node->prev = prev_node->prev;
+            prev_node->next = temp;
+
+            if (temp)
+                temp->prev = prev_node;
+
+            node->next = prev_node;
+            prev_node->prev = node;
+
+            prev_node = node->prev;
+
+            /* Print the list after each swap */
+            listint_t *current = *list;
+            while (current)
+            {
+                if (current->next)
+                    printf("%d, ", current->n);
+                else
+                    printf("%d\n", current->n);
+                current = current->next;
+            }
+        }
+        node = next_node;
+    }
 }
