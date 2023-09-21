5f4bd92e365fcd0ae7b438818a9199dda0c146b5
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-21 09:25:48 +0200

    Create and update sort.h

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/*	Print	*/ 
+/* Function prototype for print_array */
+void print_array(const int *array, size_t size);
+/* Function prototype for print_list */
+void print_list(const listint_t *list);
+
+/*	Sorting	*/
+/* Function prototype for bubble_sort */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
