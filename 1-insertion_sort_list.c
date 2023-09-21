284028b17afd06bbc3aa3c723b299ca9f0ab5aaf
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-21 23:48:24 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,38 +2,52 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list in ascending order
+ * using the Insertion sort algorithm
  *
  * @list: Pointer to a pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node = NULL, *tmp = NULL;
+    listint_t *current, *sorted, *next;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	node = *list;
-	node = node->next;
-	while (node)
-	{
-		while (node->prev && node->n < (node->prev)->n)
-		{
-			tmp = node;
-			if (node->next)
-				(node->next)->prev = tmp->prev;
-			(node->prev)->next = tmp->next;
-			node = node->prev;
-			tmp->prev = node->prev;
-			tmp->next = node;
-			if (node->prev)
-				(node->prev)->next = tmp;
-			node->prev = tmp;
-			if (tmp->prev == NULL)
-				*list = tmp;
-			print_list(*list);
-			node = node->prev;
-		}
-		node = node->next;
-	}
+    sorted = NULL;
+    current = *list;
 
+    while (current)
+    {
+        next = current->next;
+
+        if (sorted == NULL || sorted->n >= current->n)
+        {
+            current->next = sorted;
+            current->prev = NULL;
+
+            if (sorted)
+                sorted->prev = current;
+
+            sorted = current;
+        }
+        else
+        {
+            listint_t *temp = sorted;
+
+            while (temp->next && temp->next->n < current->n)
+                temp = temp->next;
+
+            current->next = temp->next;
+
+            if (temp->next)
+                temp->next->prev = current;
+
+            temp->next = current;
+            current->prev = temp;
+        }
+
+        *list = sorted;
+        current = next;
+        print_list(*list);
+    }
 }
