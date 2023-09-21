3a7f20233f49938f59b45e2d20e425b6a3cb3b1b
N <51336368+Nmcleon@users.noreply.github.com>
2023-09-22 00:03:51 +0200

    final commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,51 +4,43 @@
  * insertion_sort_list - Sorts a doubly linked list in ascending order
  *
  * @list: Pointer to a pointer to the head of the list
+ * Return: void (SUCCESS)
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node, *next_node, *prev_node, *temp;
+	listint_t *node, *next_node;
 
-    if (!list || !(*list))
-        return;
+	*node = NULL;
+	*next_node = NULL;
 
-    node = (*list)->next;
-    while (node)
-    {
-        next_node = node->next;
-        prev_node = node->prev;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-        while (prev_node && prev_node->n > node->n)
-        {
-            temp = node->next;
-
-            if (prev_node->prev)
-                prev_node->prev->next = node;
-            else
-                *list = node;
-
-            node->prev = prev_node->prev;
-            prev_node->next = temp;
-
-            if (temp)
-                temp->prev = prev_node;
-
-            node->next = prev_node;
-            prev_node->prev = node;
-
-            prev_node = node->prev;
-
-            /* Print the list after each swap */
-            listint_t *current = *list;
-            while (current)
-            {
-                if (current->next)
-                    printf("%d, ", current->n);
-                else
-                    printf("%d\n", current->n);
-                current = current->next;
-            }
-        }
-        node = next_node;
-    }
+	node = *list;
+	node = node->next;
+	while (node)
+		
+	{
+		while (node->prev && node->n < (node->prev)->n)
+		{
+			next_node = node;
+			
+			if (node->next)
+				(node->next)->prev = next_node->prev;
+			(node->prev)->next = next_node->next;
+			
+			node = node->prev;
+			next_node->prev = node->prev;
+			next_node->next = node;
+			
+			if (node->prev)
+				(node->prev)->next = next_node;
+			node->prev = next_node;
+			if (next_node->prev == NULL)
+				*list = next_node;
+			print_list(*list);
+			node = node->prev;
+		}
+		node = node->next;
+	}
 }
